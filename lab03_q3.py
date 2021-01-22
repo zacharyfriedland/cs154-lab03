c3faaa0d2cb870968bf8aabc5110cc5cd5ee1b1a
zacharyfriedland <zacharyfriedland@ucsb.edu>
2021-01-22 15:40:11 -0800

    lab03_q3 finished

--- lab03_q3.py
+++ lab03_q3.py
@@ -7,16 +7,48 @@ import pyrtl
 # conditional assignment.
 
 # Declare data inputs
-# < add your code here >
+a = pyrtl.Input(bitwidth=3, name='a')
+b = pyrtl.Input(bitwidth=3, name='b')
+c = pyrtl.Input(bitwidth=3, name='c')
+d = pyrtl.Input(bitwidth=3, name='d')
+e = pyrtl.Input(bitwidth=3, name='e')
+
 
 # Declare control inputs
 s = pyrtl.Input(bitwidth=3, name='s')
 
 # Declare outputs 
-# < add your code here >
+o = pyrtl.Output(bitwidth=3, name='o')
 
 # Describe your 5:1 MUX implementation
-# < add your code here >
+with pyrtl.conditional_assignment:
+    with s == 0b000:
+        o |= a
+    with s == 0b001:
+        o |= b
+    with s == 0b010:
+        o |= c
+    with s == 0b011:
+        o |= d
+    with pyrtl.otherwise:
+        o |= e
 
-# Simulate and test your design for 16 cycles using random inputs
-# < add your code here >
+# DO: Simulate and test your design for 16 clock cycles using random inputs
+# Simulate and test your design for 8 clock cycles using random inputs
+sim_trace = pyrtl.SimulationTrace()
+sim = pyrtl.Simulation(tracer=sim_trace)
+
+import random
+for cycle in range(16):
+    # Call "sim.step" to simulate each clock cycle of the design
+    sim.step({
+        'a': 0b000,
+        'b': 0b001,
+        'c': 0b010,
+        'd': 0b011,
+        'e': 0b100,
+        's': random.choice([0b000, 0b001, 0b010, 0b011, 0b100])
+    })
+
+print('--- 3-bit 5:1 MUX Simulation -- Built using PyRTL conditionals ---')
+sim_trace.render_trace()
