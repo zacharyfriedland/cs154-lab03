c2171d1ad93d163a4b527f9488b34332f606986a
zacharyfriedland <zacharyfriedland@ucsb.edu>
2021-01-22 15:12:33 -0800

    lab03_q2 finished

--- lab03_q2.py
+++ lab03_q2.py
@@ -3,20 +3,28 @@
 import pyrtl
 
 # As any Boolen circuit, a MUX can be implemented only with the use of AND, OR, 
-# and NOT gates. Your first task is to built an 1-bit 2:1 MUX out of these gates
+# and NOT gates. Your first task is to Build a 1-bit 2:1 MUX out of these gates
 # and then simulate it for 8 cycles using random inputs as above.
 
 # Declare two 1-bit data inputs: a, b
-# < add your code here >
+a = pyrtl.Input(bitwidth=1, name='a')
+b = pyrtl.Input(bitwidth=1, name='b')
 
 # Declare one 1-bit control input: s
-# < add your code here >
+s = pyrtl.Input(bitwidth=1, name='s')
 
 # Declare one 1-bit output: o_wg
-# < add your code here >
+o_wg = pyrtl.Output(bitwidth=1, name='o_wg')
 
 # 2:1 MUX implementation using only AND, OR, and NOT gates
-# < add your code here > 
+and1_res = pyrtl.WireVector(bitwidth=1, name='A & ~S')
+and2_res = pyrtl.WireVector(bitwidth=1, name='B & S')
+
+
+and1_res <<= a & (~s)
+and2_res <<= b & s
+o_wg <<= and1_res | and2_res
+
 
 # Simulate and test the design for 8 clock cycles using random inputs
 sim_trace = pyrtl.SimulationTrace()
